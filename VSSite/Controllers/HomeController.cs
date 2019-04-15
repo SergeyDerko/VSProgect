fc38c21e01953842345c87959a91015391ed46db
SergeyDerko <sergeyderko@gmail.com>
2019-04-15 17:55:34 +0300

    fix

--- VSSite/Controllers/HomeController.cs
+++ VSSite/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using VSCore.Concrete;
+using VSCore.Entity;
 
 namespace VSSite.Controllers
 {
@@ -7,6 +10,83 @@ namespace VSSite.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public string DataSid()
+        {
+            using (Context context = new Context())
+            {
+                if (!context.Businesses.Any())
+                {
+                    #region Категории бизнессов
+
+                    context.CategoryBusnesses.Add(new CategoryBusness
+                    {
+                        Name = "IT категория",
+                        NameEn = "IT Category"
+                    });
+                    context.CategoryBusnesses.Add(new CategoryBusness
+                    {
+                        Name = "Еда",
+                        NameEn = "Food"
+                    });
+                    context.CategoryBusnesses.Add(new CategoryBusness
+                    {
+                        Name = "Услуги",
+                        NameEn = "Services"
+                    });
+
+                    context.SaveChanges();
+
+                    #endregion
+
+                    #region Бизнессы
+
+                    context.Businesses.Add(new Business
+                    {
+                        
+                    });
+                    #endregion
+
+
+
+                    #region Новости
+
+                    context.Newses.Add(new News
+                    {
+                        
+                    });
+
+
+
+                    #endregion
+
+                    #region Партнерка
+
+                    context.Partners.Add(new Partner
+                    {
+                        
+                    });
+
+
+                    #endregion
+
+                    #region Рубрики полезно
+
+                    context.UsefulRubricses.Add(new UsefulRubrics
+                    {
+
+                    });
+
+
+                    #endregion
+
+
+
+                }
+            }
+
+            return "Гтово!";
         }
     }
 }
