c0d16d0678766940fddcdf6709cb740d15721acb
garluh <garluh@gmail.com>
2019-04-15 17:49:22 +0300

    Добавлена простая реализация редактора новостей

--- VSSite/Controllers/NewsController.cs
+++ VSSite/Controllers/NewsController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Telerik.Windows.Documents.Flow.FormatProviders.Html;
+using Telerik.Windows.Documents.Flow.Model;
 using VSCore.Concrete;
+using VSCore.Entity;
 
 namespace VSSite.Controllers
 {
@@ -17,13 +19,62 @@ namespace VSSite.Controllers
             return View(db.Newses.OrderByDescending(x=>x.DateNews));
         }
 
-        [HttpPost]
-        public ActionResult Save(string title, string titleEn, string body, string bodyEn, string detailUrl, List<string> videoUrl)
+
+        public ActionResult NewsAdd()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(string titleUA, string titleEn, string body, string bodyEn, string detailUrl, List<string> videoUrl)
+        {
+
             string bodyHtml = HttpUtility.HtmlDecode(body);
             string bodyHtmlEn = HttpUtility.HtmlDecode(bodyEn);
             HtmlFormatProvider htmlProvider = new HtmlFormatProvider();
-            return null;
+            RadFlowDocument document = htmlProvider.Import(bodyHtml);
+            RadFlowDocument documentEn = htmlProvider.Import(bodyHtmlEn);
+            using (Context db = new Context())
+            {
+                var vidData = new List<Video>();
+
+                foreach (var x in vidData)
+                {
+                    vidData.Add(new Video
+                    {
+                        Url = x.Url
+                    });
+                }
+
+              db.Newses.Add(new News
+              {
+                  Title = titleUA,
+                  TitleEn = titleEn,
+                  DateNews = DateTime.Now,
+                  Body = htmlProvider.Export(document),
+                  BodyEn = htmlProvider.Export(documentEn),
+                  DetailUrl = detailUrl,
+                  Videos = vidData
+              });
+              db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (Context context = new Context())
+            {
+                var toRemove = context.Newses.FirstOrDefault(x => x.NewsId == id);
+                if (toRemove != null)
+                {
+                    context.Newses.Remove(toRemove);
+                    context.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
         }
 
     }
