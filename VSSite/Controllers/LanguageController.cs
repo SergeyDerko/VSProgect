79bf09e3a69fc93453270aa9834961d661681056
SergeyDerko <sergeyderko@gmail.com>
2019-04-15 15:49:18 +0300

    язык добавлен

---
+++ VSSite/Controllers/LanguageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VSSite.Controllers
+{
+    public class LanguageController : Controller
+    {
+        /// <summary>
+        /// Смена языка
+        /// </summary>
+        /// <param name="language">Язык</param>
+        /// <returns></returns>
+        public ActionResult ChoiceOfLanguage(string language)
+        {
+            if (language != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("ru");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie("Language") { Value = language };
+            Response.Cookies.Add(cookie);
+            return Redirect("/Home/Index");
+        }
+    }
+}
--- VSSite/Global.asax.cs
+++ VSSite/Global.asax.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +18,20 @@ namespace VSSite
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpCookie cookie = HttpContext.Current.Request.Cookies["Language"];
+            if (cookie?.Values != null)
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("ru");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(cookie.Value);
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("ru");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru");
+            }
         }
     }
 }
