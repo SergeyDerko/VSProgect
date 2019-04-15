3ac1b362c8de73472901d725a43172dc12a87c5d
garluh <garluh@gmail.com>
2019-04-15 15:56:11 +0300

    telerik dll add

---
+++ VSCore/Concrete/Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSCore.Entity;
+
+namespace VSCore.Concrete
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DefaultConnection")
+        {
+        }
+
+        public virtual DbSet<Video> Videos { get; set; }
+        public virtual DbSet<Branch> Branches  { get; set; }
+        public virtual DbSet<Business> Businesses { get; set; }
+        public virtual DbSet<CategoryBusness> CategoryBusnesses { get; set; }
+        public virtual DbSet<Newsletter> Newsletters { get; set; }
+        public virtual DbSet<News> Newses { get; set; }
+        public virtual DbSet<TypesBusinesses> TypesBusnesses { get; set; }
+        public virtual DbSet<Partner> Partners { get; set; }
+        public virtual DbSet<Useful> Usefuls { get; set; }
+        public virtual DbSet<UsefulRubrics> UsefulRubricses { get; set; }
+    }
+
+}
