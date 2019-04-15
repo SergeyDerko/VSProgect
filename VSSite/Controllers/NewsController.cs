19956d37f097cb873100ec3cd8f7f417d49f7224
garluh <garluh@gmail.com>
2019-04-15 16:15:11 +0300

    telerik dll add 2

--- VSSite/Controllers/NewsController.cs
+++ VSSite/Controllers/NewsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Telerik.Windows.Documents.Flow.FormatProviders.Html;
+using VSCore.Concrete;
 
 namespace VSSite.Controllers
 {
@@ -11,7 +13,18 @@ namespace VSSite.Controllers
         // GET: News
         public ActionResult Index()
         {
-            return View();
+            Context db = new Context();
+            return View(db.Newses.OrderByDescending(x=>x.DateNews));
         }
+
+        [HttpPost]
+        public ActionResult Save(string title, string titleEn, string body, string bodyEn, string detailUrl, List<string> videoUrl)
+        {
+            string bodyHtml = HttpUtility.HtmlDecode(body);
+            string bodyHtmlEn = HttpUtility.HtmlDecode(bodyEn);
+            HtmlFormatProvider htmlProvider = new HtmlFormatProvider();
+            return null;
+        }
+
     }
 }
