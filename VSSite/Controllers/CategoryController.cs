5669a35a5ae6f9ab229807af857b8d30551a836e
garluh <garluh@gmail.com>
2019-04-17 10:55:28 +0300

    редактор категорий

--- VSSite/Controllers/CategoryController.cs
+++ VSSite/Controllers/CategoryController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.Linq;
+ using System.IO;
+ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
@@ -83,6 +84,52 @@ namespace VSSite.Controllers
             }
 
             return Json(new[] { categoryBusness }.ToDataSourceResult(request, ModelState));
+        }
+
+
+        public ActionResult Async_Save(IEnumerable<HttpPostedFileBase> files)
+        {
+            var name = Guid.NewGuid().ToString();
+            string fullFile = "";
+            foreach (var file in files)
+            {
+
+                if (file != null)
+                {
+                    //var fileName = Path.GetFileName(file.FileName);
+
+                    var ext = Path.GetExtension(file.FileName);
+                    var physicalPath = Path.Combine(Server.MapPath("~/Images/Category"), $"{name}{ext}");
+                    fullFile = $"{name}{ext}";
+                    file.SaveAs(physicalPath);
+                }
+            }
+
+            return Json(new { fileName = fullFile });
+        }
+
+        public ActionResult Async_Remove(string[] fileNames)
+        {
+            // The parameter of the Remove action must be called "fileNames"
+
+            if (fileNames != null)
+            {
+                foreach (var fullName in fileNames)
+                {
+                    var fileName = Path.GetFileName(fullName);
+                    var physicalPath = Path.Combine(Server.MapPath("~/Images/Category"), fileName);
+
+                    // TODO: Verify user permissions
+
+                    if (System.IO.File.Exists(physicalPath))
+                    {
+                        System.IO.File.Delete(physicalPath);
+                    }
+                }
+            }
+
+            // Return an empty string to signify success
+            return Content("");
         }
 
         protected override void Dispose(bool disposing)
